c5e0cff24da8f819adfceec49484ddb0ec34dbca
Wectornanime <wectornanime@gmail.com>
2024-09-25 15:20:55 -0300

    update project

---
+++ login.js
@@ -0,0 +1,37 @@
+let usuarios = [];
+
+// Função para carregar os dados do JSON
+async function carregarUsuarios() {
+    const response = await fetch('users.json');  // Faz o fetch do arquivo JSON
+    usuarios = await response.json();
+}
+
+// Função para validar o login
+function validarLogin(event) {
+    event.preventDefault(); // Previne o envio padrão do formulário
+    
+    // Pega os valores dos inputs
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    
+    // Busca um usuário correspondente no JSON
+    const usuarioValido = usuarios.usuarios.find(user => user.username === username && user.password === password);
+    
+    if (usuarioValido) {
+        alert('Login realizado com sucesso!');
+        // Redireciona para a página index.html
+        window.location.href = "reserve.html";
+    } else {
+        // Mostra mensagem de erro se a validação falhar
+        document.getElementById('mensagemErro').style.display = 'block';
+    }
+}
+
+window.onload = function() {
+    // Carrega os usuários do arquivo JSON
+    carregarUsuarios();
+    
+    // Adiciona o evento de envio ao formulário de login
+    const loginForm = document.getElementById('loginForm');
+    loginForm.addEventListener('submit', validarLogin);
+};
